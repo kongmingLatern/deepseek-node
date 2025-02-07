2dc9cf56ee01bc90209490b5c3d32a660463733d
风之兮原 <1577310596@qq.com>
2025-02-07 14:12:53 +0800

    chore: add interception

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
